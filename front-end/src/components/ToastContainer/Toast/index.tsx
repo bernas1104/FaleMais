ef6803fa2f4a9a0f9bf6a7a51aabda8170e3027d
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-09-04 17:01:40 -0300

    Toast auto and manual remove added

--- front-end/src/components/ToastContainer/Toast/index.tsx
+++ front-end/src/components/ToastContainer/Toast/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FiAlertTriangle, FiX } from 'react-icons/fi';
 
 import { Container, Content } from './styles';
-import { ToastMessage } from '../../../hooks/ToastContext';
+import { ToastMessage, useToast } from '../../../hooks/ToastContext';
 
 interface ToastProps {
   message: ToastMessage;
@@ -10,6 +10,18 @@ interface ToastProps {
 }
 
 const Toast: React.FC<ToastProps> = ({ message, style }) => {
+  const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [removeToast, message.id]);
+
   return (
     <Container style={style}>
       <FiAlertTriangle size={20} color="#f5f5f5" />
@@ -18,7 +30,7 @@ const Toast: React.FC<ToastProps> = ({ message, style }) => {
         {message.description && <p>{message.description}</p>}
       </Content>
 
-      <button type="button">
+      <button type="button" onClick={() => removeToast(message.id)}>
         <FiX size={20} color="#f5f5f5" />
       </button>
     </Container>
